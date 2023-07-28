c8cea8fb8d80bf3cbb1739daee8b9743d72ef3a4
Carlos Henrique dias Akim <ch.akim1994@gmail.com>
2023-07-28 10:36:34 -0300

    atualizando

--- Lista de SuperMercado/Script.js
+++ Lista de SuperMercado/Script.js
@@ -1,17 +1,40 @@
 var item = []
 
-document.querySelector('input[type=submit]')
-.addEventListener('click',()=>{
-    var produto = document.querySelector('input[name=nome-produto]').value
-    var valorProduto = document.querySelector('input[name=valor-produto]').value
 
+document.querySelector('input[type=submit]').addEventListener('click',()=>{
+    var produto = document.querySelector('input[name=nome-produto]')
+    var valorProduto = document.querySelector('input[name=valor-produto]')
+    var resultado = document.querySelector("#Resultado")
     item.push({
-        nome: produto,
-        valor: valorProduto
+        nome: produto.value,
+        valor: valorProduto.value
 
     })
-    
-    
 
+    var tab = document.querySelector("#Lista-produtos")
+    
+    //comando .map funcionou como um loop.
+    tab.innerHTML = ""
+    soma = 0
+    item.map(function(val){
+        
+        soma+=parseFloat(val.valor)
+        tab.innerHTML+=`
+        
+        <div id="Lista-produtos-single">
+            <h3 id="produto">${val.nome}</h3>
+            <h3 id="valor"><span>${val.valor}</span></h3>
+        </div>
+        `
+        resultado.innerHTML = `R$${soma}`
+        
+    })
+
+    produto.value = ""
+    valorProduto.value = ""
+        
+    
+    
 
 })
+

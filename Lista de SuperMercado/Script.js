67120439cd3c5b6fb1971afd631948eccf1e7447
Carlos Henrique dias Akim <ch.akim1994@gmail.com>
2023-07-29 15:50:03 -0300

    atualização

--- Lista de SuperMercado/Script.js
+++ Lista de SuperMercado/Script.js
@@ -1,40 +1,39 @@
 var item = []
 
+document.querySelector('input[type=submit]').addEventListener('click',function(){
+    var produto = document.querySelector("input[name=nome-produto]")
+    var valorProduto = document.querySelector("input[name=valor-produto]")
+    var lista = document.querySelector("#Lista-produtos")
+    var res = document.querySelector("#Resultado")
 
-document.querySelector('input[type=submit]').addEventListener('click',()=>{
-    var produto = document.querySelector('input[name=nome-produto]')
-    var valorProduto = document.querySelector('input[name=valor-produto]')
-    var resultado = document.querySelector("#Resultado")
     item.push({
         nome: produto.value,
-        valor: valorProduto.value
-
-    })
-
-    var tab = document.querySelector("#Lista-produtos")
-    
-    //comando .map funcionou como um loop.
-    tab.innerHTML = ""
-    soma = 0
+        valor: valorProduto.value,
+    });
+    lista.innerHTML=''
+    soma=0
+    produto.focus()
     item.map(function(val){
-        
         soma+=parseFloat(val.valor)
-        tab.innerHTML+=`
-        
+        produto.value=' '
+        valorProduto.value=' '
+        lista.innerHTML+=`
         <div id="Lista-produtos-single">
             <h3 id="produto">${val.nome}</h3>
-            <h3 id="valor"><span>${val.valor}</span></h3>
-        </div>
+            <h3 id="valor"><span>R$${val.valor}</span></h3>
         `
-        resultado.innerHTML = `R$${soma}`
-        
+        res.innerHTML=`Total: R$${soma}`        
     })
 
-    produto.value = ""
-    valorProduto.value = ""
-        
-    
     
 
 })
 
+function limpar(){
+    item = []
+    var lista = document.querySelector("#Lista-produtos").innerHTML=''
+    var res = document.querySelector("#Resultado").innerHTML=`Total: R$0`
+
+    
+}
+    

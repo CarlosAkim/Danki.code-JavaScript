82a8d31f0fffb05f96c1b376de79f9eb0a847e68
Carlos Henrique dias Akim <140402022+CarlosAkim@users.noreply.github.com>
2023-08-09 14:05:55 -0300

    atualização

--- Pokedex/Script.js
+++ Pokedex/Script.js
@@ -1,3 +1,54 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-.then(response=>response.json())
-.then(pokemon=>console.log(pokemon))
+var box = document.querySelector("#Pokemon-box")
+pokemon = []
+
+function quantidades(){
+
+    var qtd = document.querySelector("#selecao").value
+    box.innerHTML=""
+
+    qtdPokemon(qtd)
+}
+
+function qtdPokemon(quantidade){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade).then(response=>response.json()).then(allpokemons => {
+        // aqui começamos a mapear para puxar os nomes e imagems dentro do nossa biblioteca
+       allpokemons.results.map((val)=>{
+           
+           fetch(val.url).then(response=>response.json()).then(imagem=> {            
+               //acrescetamos o nome, image, e imagem shiny dentro da biblioteca.
+               pokemon.push({nome:val.name,img:imagem.sprites.front_default,shiny:imagem.sprites.front_shiny})
+               
+               // acrescetamos o lent
+               if(pokemon.length ==quantidade){
+                   pokemon.map((val)=>{
+                       box.innerHTML+=`
+                       <div id="pokemon"><!--Pokemons img-->
+                       <img src="${val.img}" alt="Imagem dos pokemons">
+                       <p>${val.nome}</p>
+                       </div>`
+                 
+                    })
+                    
+               }
+             
+                 
+           })   
+               
+               
+       })
+   })
+   
+}
+
+    
+   
+       
+
+   
+
+
+
+/*`<div id="pokemon"><!--Pokemons img-->
+                    <img src="${val.imagem}" alt="Imagem dos pokemons">
+                    <p>${val.nome}</p>
+                </div>` */

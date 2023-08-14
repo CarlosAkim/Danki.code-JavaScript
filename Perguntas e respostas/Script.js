fd1f447d4e7cedf1e2ad587435d4923bba6680e6
Carlos Henrique dias Akim <140402022+CarlosAkim@users.noreply.github.com>
2023-08-14 16:19:51 -0300

    Projeto novo
    Novo projeto, para o repositorio

---
+++ Perguntas e respostas/Script.js
@@ -0,0 +1,17 @@
+//Começamos puxando todo o conteudo do tipo 'radio' e atribuimos a variavel 'elementos'
+var elementos = document.querySelectorAll('input[type=radio]')
+
+//agora vamos criar um evento para que seja notificado alguma mudança na escolha do usuario.
+for( var i in elementos){
+    //usamos o comando 'change' para que toca vez que tenha uma troca nas opções ele acione a função
+    elementos[i].addEventListener('change',(e)=>{
+        
+        let marcado = e.target.value
+
+        if(marcado == 'correta'){
+            let el = e.target.previousElementSibling.style.backgroundColor = 'green'
+        }else {
+            let el = e.target.previousElementSibling.style.backgroundColor = 'red'
+        }
+    })
+}

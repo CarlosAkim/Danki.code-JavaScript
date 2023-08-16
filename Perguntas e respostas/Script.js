c6f2b3234a98a97c6da8b3d0c9d736c66294ab98
Carlos Henrique dias Akim <140402022+CarlosAkim@users.noreply.github.com>
2023-08-16 09:44:28 -0300

    Atualizando o codigo.

--- Perguntas e respostas/Script.js
+++ Perguntas e respostas/Script.js
@@ -1,33 +1,30 @@
-//Começamos puxando todo o conteudo do tipo 'radio' e atribuimos a variavel 'elementos'
 var elementos = document.querySelectorAll('[type=radio]')
 
-//agora vamos criar um evento para que seja notificado alguma mudança na escolha do usuario.
-for( var i=0;i<elementos.length;i++){
-    //usamos o comando 'change' para que toca vez que tenha uma troca nas opções ele acione a função
+for (var i in elementos){
     elementos[i].addEventListener('change',(e)=>{
-        
         let marcado = e.target.value
 
-        if(marcado == 'correta'){
+        if (marcado == 'correta'){
             // resposta correta
-            let verde = e.target.parentNode;
-            verde.style.background = 'green'
+            let verde = e.target.parentNode
+            verde.style.backgroundColor = 'green'
 
-            let els = document.querySelectorAll('[type=radio]')
-            for(let n = 0 ; n < els.length; n++){
-                els[n].disabled = true;
+            let ele = verde.parentNode.querySelectorAll('[type=radio]')
+            for(var n in ele){
+                ele[n].disabled = true;
             }
-        }else if(marcado == 'incorreta') {
+        }else if(marcado=='incorreta'){
             //resposta incorreta
             let vermelho = e.target.parentNode
-            vermelho.style.background = 'red'
+            vermelho.style.backgroundColor = 'red'
 
-            let els = document.querySelectorAll('[type=radio]')
-            for(let n in els){
-                els[n].disabled=true
+            let ele = vermelho.parentNode.querySelectorAll('[type=radio]')
+            for(var n in ele){
+                ele[n].disabled = true;
             }
-
-
+            var correto = vermelho.parentNode.querySelector('[value=correta]')
+            correto.parentNode.style.backgroundColor='green'
         }
+
     })
 }
